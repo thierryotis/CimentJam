5539ac6230f1f22a44b5052d39d0741e5173d0a4
thierryotis <thierryotis@yahoo.fr>
2023-05-20 17:12:49 +0200

    correction of App.js

--- CimentJam/frontend/src/App.js
+++ CimentJam/frontend/src/App.js
@@ -8,49 +8,6 @@ import ThemeProvider from './theme';
 import { StyledChart } from './component/chart';
 import ScrollToTop from './component/scroll-to-top';
 
-
-import AddChargement from "./component/chargement/AddChargement";
-import GetChargements from "./component/chargement/GetChargements";
-
-import AddDechargement from "./component/dechargement/Adddechargement";
-import GetDechargements from "./component/dechargement/GetDechargements";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}  />
-        <Route path="/addproprio" element={<AddProprio />}  />
-        <Route path="/getproprios" element={<GetProprios />}  />
-        <Route path="/addchauffeur" element={<AddChauffeur />}  />
-        <Route path="/getchauffeurs" element={<GetChauffeurs />}  />
-        <Route path="/addcamion" element={<AddCamion />}  />
-        <Route path="/getcamions" element={<GetCamions />}  />
-        <Route path="/addproduit" element={<AddProduit />}  />
-        <Route path="/getproduits" element={<GetProduits />}  />
-
-        <Route path="/addchargement" element={<AddChargement />}  />
-        <Route path="/getchargements" element={<GetChargements />}  />
-
-        <Route path="/adddechargement" element={<AddDechargement />}  />
-        <Route path="/getdechargements" element={<GetDechargements />}  />
-
-
-      </Routes>
-      <ToastContainer
-        position="bottom-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-    </BrowserRouter>
-=======
 // ----------------------------------------------------------------------
 
 export default function App() {
@@ -64,6 +21,5 @@ export default function App() {
         </ThemeProvider>
       </BrowserRouter>
     </HelmetProvider>
->>>>>>> addingTemplate
   );
 }
