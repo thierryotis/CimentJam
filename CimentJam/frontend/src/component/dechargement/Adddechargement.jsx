1ea47583bc6e9810907fa324766cc284b7e0c033
thierryotis <thierryotis@yahoo.fr>
2023-05-20 17:10:53 +0200

    test commit master

--- CimentJam/frontend/src/component/dechargement/Adddechargement.jsx
+++ CimentJam/frontend/src/component/dechargement/Adddechargement.jsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
-=======
 import React, { useState } from 'react';
->>>>>>> addingTemplate
 import { Button, TextField, Typography, Container } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { toast } from 'react-toastify';
