4e4ea19d39938e017ff2dba65d69cf5ba7f751de
thierryotis <thierryotis@yahoo.fr>
2023-05-31 01:34:17 +0200

    created a local and an online file for database

--- CimentJam/backend/db/Database - local.js
+++ CimentJam/backend/db/Database - local.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2/promise');
 const connectDatabase = async () => {
   try {
     const connection = await mysql.createConnection({
-      host: "syfegrouyz447.mysql.db",
+      host: "127.0.0.1",
       port: 3306,
-      user: "syfegrouyz447",
-      password: "SKYdfRGcQ6HT",
-      database: "syfegrouyz447",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     });
 
     console.log('Connected to the database!');
