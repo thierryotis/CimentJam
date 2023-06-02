58c52bec2d6e67ac8c782c24e52f1098d0a0ca6f
thierryotis <thierryotis@yahoo.fr>
2023-06-02 19:55:19 +0200

    listened port modified on express 4

--- CimentJam/backend/app.js
+++ CimentJam/backend/app.js
@@ -22,7 +22,7 @@ const corsOptions ={
 app.use(cors(corsOptions));
 //app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", corsOptions.origin);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", corsOptions.credentials);
