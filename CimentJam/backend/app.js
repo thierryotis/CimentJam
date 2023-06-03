ea0d2fdeff65c42c20fe85ed6859a94071cb9c67
thierryotis <thierryotis@yahoo.fr>
2023-06-03 13:54:29 +0200

    setting the origin from req.headers

--- CimentJam/backend/app.js
+++ CimentJam/backend/app.js
@@ -25,11 +25,10 @@ app.use((req, res, next) => {
   const allowedOrigins = ["https://www.nomothierry.com", "https://nomothierry.com", "http://www.nomothierry.com", "http://nomothierry.com"];
   const { origin } = req.headers;
   
-  /*if (allowedOrigins.includes(origin)) {
+  if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
-  }*/
-  console.log(origin)
-  res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  
 
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
