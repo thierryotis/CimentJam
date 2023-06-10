b74808f3a94040b41fb6781c40a0753e837d7660
thierryotis <thierryotis@yahoo.fr>
2023-06-10 16:41:33 +0200

    adding another client domain

--- CimentJam/backend/app.js
+++ CimentJam/backend/app.js
@@ -21,7 +21,7 @@ const corsOptions ={
 //app.use(cors(corsOptions));
 //app.use(cors());
 app.use((req, res, next) => {
-  const allowedOrigins = ["https://cimentjamfrontend.onrender.com"];
+  const allowedOrigins = ["https://cimentjamfrontend.onrender.com","https://nomothierry.com", "https://www.nomothierry.com"];
   const { origin } = req.headers;
   
   if (allowedOrigins.includes(origin)) {
