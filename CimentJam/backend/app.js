70309d2d0be4d9ed19c78d29626819bbed3bc89e
thierryotis <thierryotis@yahoo.fr>
2023-06-10 16:07:21 +0200

    return to a sooner version

--- CimentJam/backend/app.js
+++ CimentJam/backend/app.js
@@ -23,13 +23,12 @@ const corsOptions ={
 app.use((req, res, next) => {
   const allowedOrigins = ["https://cimentjamfrontend.onrender.com"];
   const { origin } = req.headers;
-  console.log(req.headers)
   
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
   
-  res.setHeader("Access-Control-Allow-Origin", "https://cimentjamfrontend.onrender.com")
+
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
